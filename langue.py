e072b40638020f2056c7cb8fead06e76b50245d7
Tchuidjami <107814307+Tchuidjami@users.noreply.github.com>
2024-02-15 01:14:10 +0100

    Update langue.py

--- langue.py
+++ langue.py
@@ -1,12 +1,12 @@
 import whisper
 
 # code qui permet d extraire la video en audio
-#ffmpeg -i smart.mp4 -vn -ar 44100 -ac 2 -ab 320 smart.wav 
+#ffmpeg -i input.mp4 -vn -ar 44100 -ac 2 -ab 320 input.mp3 
 
 model = whisper.load_model("base")
 
 # load audio and pad/trim it to fit 30 seconds
-audio = whisper.load_audio("test_compress.wav")
+audio = whisper.load_audio("test.mp3")
 audio = whisper.pad_or_trim(audio)
 
 # make log-Mel spectrogram and move to the same device as the model
