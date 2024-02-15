fcfd56b884edf7e257b0814d07aa3d580229c653
Tchuidjami <107814307+Tchuidjami@users.noreply.github.com>
2024-02-15 01:15:09 +0100

    Update subtiles.py

--- subtiles.py
+++ subtiles.py
@@ -1,7 +1,7 @@
 import assemblyai as aai
 
 aai.settings.api_key = "5d154d75227e45edb2a466c178ea89e0"
-transcript = aai.Transcriber().transcribe("test_compress.wav")
+transcript = aai.Transcriber().transcribe("test.mp3")
 
 subtitles=transcript.export_subtitles_srt()
 
