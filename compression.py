95a62e17d78aa68d4e31f11c1849b72fb56cd3a6
Tchuidjami <107814307+Tchuidjami@users.noreply.github.com>
2024-02-15 01:12:50 +0100

    Update compression.py

--- compression.py
+++ compression.py
@@ -3,5 +3,5 @@ import subprocess
 import ffmpeg
 
 
-ffmpeg_cmd= f'ffmpeg -i {"smart.mp4"} -vf scale=634:356 {"smart_out.mp4"}'
+ffmpeg_cmd= f'ffmpeg -i {"test.mp4"} -vf scale=634:356 {"test_compress.mp4"}'
 subprocess.run(ffmpeg_cmd, shell=True)
